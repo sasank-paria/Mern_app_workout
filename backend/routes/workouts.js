3ac5e4d59c1d7aa514e25ab5d8cfc0d6f7d8cea1
paria <83511327+sasank-paria@users.noreply.github.com>
2023-12-12 01:30:30 +0530

    .

---
+++ backend/routes/workouts.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const{
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutcontroller')
+
+const router = express.Router()
+
+
+//get all workouts
+router.get('/',getWorkouts)
+
+//get single workout
+router.get('/:id',getWorkout)
+
+
+//post a single workout
+router.post('/',createWorkout) 
+
+
+//delete a workout
+router.delete('/:id',deleteWorkout)
+
+//update a workout
+router.patch('/:id',updateWorkout)
+
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,35 @@
 require('dotenv').config()
-
 const express = require('express')
+const mongoose = require('mongoose')
 
+const workoutRoutes = require('./routes/workouts')
 
 //starting express app
 const app = express()
 
-//routes
-app.get('/',(req,res)=>{
-    res.json({mssg:'welcome to the app'})
+//to use json data
+app.use(express.json())
+
+
+//middleware
+app.use((req,res,next)=>{
+    console.log("middleware")
+    next();
 })
 
+//routes
+app.use('/api/workouts',workoutRoutes)
+
+
+
+//connect to database
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
 //listen for requests
 app.listen(process.env.PORT, ()=>{
-    console.log('listening on port 4000')
+    console.log('connected to database , listening on port 4000')
 }) 
+})
+.catch((error)=>{
+    console.log(error)
+})
